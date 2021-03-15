4000556dff507772a09e311f98abacc2a9e52d08
MarcBad98 <badi_mar@outlook.com>
2021-03-14 23:27:06 -0400

    spelling fix

--- app/graphql/queries.py
+++ app/graphql/queries.py
@@ -8,7 +8,7 @@ class Query(graphene.ObjectType):
         objects.UserType,
         search=graphene.String(),
     )
-    get_friend_requests = graphene.List(
+    get_friends_requests = graphene.List(
         objects.FriendsRequestType,
         keycloak_user_id=graphene.String(required=True),
     )
@@ -21,7 +21,7 @@ class Query(graphene.ObjectType):
         # pylint: disable=no-member
         return collections.User.objects.search_text(search)
 
-    def resolve_get_friend_requests(self, info, keycloak_user_id):
+    def resolve_get_friends_requests(self, info, keycloak_user_id):
         # pylint: disable=no-member
         return collections.FriendsRequest.objects(pairing=keycloak_user_id)
 
