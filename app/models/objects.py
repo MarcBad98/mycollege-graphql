4b0ee9cb04fbfd4bd12c26b2a348697952b4c7a2
MarcBad98 <badi_mar@outlook.com>
2021-04-06 18:28:02 -0400

    added convenient metadata field to jobtype

--- app/models/objects.py
+++ app/models/objects.py
@@ -4,6 +4,15 @@ from graphene_mongo import MongoengineObjectType
 from app.models import collections
 
 
+class MetaDataType(graphene.ObjectType):
+    """ Assortment of arbitrary fields used as metadata. """
+
+    # JobType
+    user_is_poster = graphene.Boolean()
+    user_is_applicant = graphene.Boolean()
+    user_has_saved = graphene.Boolean()
+
+
 class EmploymentSectionType(MongoengineObjectType):
     class Meta:
         model = collections.EmploymentSection
@@ -40,5 +49,28 @@ class JobApplicationType(MongoengineObjectType):
 
 
 class JobType(MongoengineObjectType):
+
+    metadata = graphene.Field(
+        MetaDataType,
+        keycloak_user_id=graphene.String(required=True),
+    )
+
+    def resolve_metadata(self, info, keycloak_user_id):
+        # pylint: disable=no-member
+        is_poster = keycloak_user_id == self.poster
+        is_applicant = keycloak_user_id in [app.applicant for app in self.applications]
+        has_saved = (
+            collections.User.objects(
+                keycloak_user_id=keycloak_user_id,
+                jobs_saved=str(self.id),
+            ).count()
+            == 1
+        )
+        return MetaDataType(
+            user_is_poster=is_poster,
+            user_is_applicant=is_applicant,
+            user_has_saved=has_saved,
+        )
+
     class Meta:
         model = collections.Job
